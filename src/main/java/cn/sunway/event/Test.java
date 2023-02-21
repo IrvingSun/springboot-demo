c593b2d53ccb4613b7d7fc2f48040cf06126af2e
sunway <sunwei.pyw@gmail.com>
2023-02-22 06:26:00 +0800

    event

--- src/main/java/cn/sunway/event/Test.java
+++ src/main/java/cn/sunway/event/Test.java
@@ -11,9 +11,25 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
-        configApplicationContext.register(Call119FireEventListener.class);
-        configApplicationContext.refresh();
-        configApplicationContext.publishEvent(new FireEvent("DDDD!!!!!!"));
+//        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
+//        configApplicationContext.register(Call119FireEventListener.class);
+//        configApplicationContext.refresh();
+//        configApplicationContext.publishEvent(new FireEvent("DDDD!!!!!!"));
+
+        AnnotationConfigApplicationContext parentApplicationContext = new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext childApplicationContext = new AnnotationConfigApplicationContext();
+        childApplicationContext.setParent(parentApplicationContext);
+
+        /**
+         * childApplicationContext注册的事件会传播到parentApplicationContext
+         */
+        parentApplicationContext.register(Call119FireEventListener.class);
+        parentApplicationContext.refresh();
+
+        childApplicationContext.register(EmergencyFireEventListener.class);
+        childApplicationContext.refresh();
+
+        childApplicationContext.publishEvent(new FireEvent("DDDD!!!!!!"));
+
     }
 }
